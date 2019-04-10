489e9efdae3a8b31b6272bb3fa733f1c35f563e2
sivanWu0222 <yirufeng@foxmail.com>
2019-04-09 22:57:43 -0700

    提交

---
+++ python_work/hello_world.py
@@ -0,0 +1,9 @@
+print("hello world interpreter")
+message = "Hello Python world!"
+print(message)
+
+message = "Hello Python Crash Course World!"
+print(message)
+
+message = "Hello Python Crash Course World!"
+print(message)
---
+++ python_work/name.py
@@ -0,0 +1,8 @@
+#coding=utf-8
+name = "a lovelace"
+print(name.title()) #将字符串中的每个单词首字母大写，其他字母小写
+
+name = "Sivan Wu"
+print(name.upper()) #将字符串全部转换为大写
+print(name.lower()) #将字符串全部转换为小写
+
